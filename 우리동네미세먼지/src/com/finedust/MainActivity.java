2a5411eca3aec38cd59fd5b9c807ee9da2c2829f
seonghak.jeong <sunghak1211@hanmail.net>
2014-08-24 17:20:24 +0900

    [#1] 좌표 받아오기

--- 우리동네미세먼지/src/com/finedust/MainActivity.java
+++ 우리동네미세먼지/src/com/finedust/MainActivity.java
@@ -1,13 +1,18 @@
 package com.finedust;
 
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
-import android.app.Fragment;
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -16,47 +21,67 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) { 
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		LocationManager locationManager;
+		
+		locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		
+		Criteria reqment = new Criteria();
+		reqment.setAccuracy(Criteria.ACCURACY_FINE);
+		reqment.setAltitudeRequired(false);
+		reqment.setBearingRequired(false);
+		reqment.setCostAllowed(true);
+		reqment.setPowerRequirement(Criteria.POWER_LOW);
+		
+		String pvder = locationManager.getBestProvider(reqment, true);
+		Location loc = locationManager.getLastKnownLocation(pvder);
+		
+		LocUpdate(loc);
+		locationManager.requestLocationUpdates(pvder, 500, 50, locListner);
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	
+	private final LocationListener locListner = new LocationListener(){
+		public void onLocationChanged(Location location){
+			LocUpdate(location);
 		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
+		public void onProviderDisabled(String provider){
+			LocUpdate(null);
 		}
+		
+		public void onProviderEnabled(String provider){}
+		public void onStatusChanged(String provider, int status, Bundle extra){}
+		
+	};
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
+	private void LocUpdate(Location loc){
+		TextView LocTV = (TextView)findViewById(R.id.LocTextView);
+		System.out.println("loctostring"+loc.toString());
+		String str= null;
+		String addrStr = null;
+		double lat = loc.getLatitude();
+		double lng = loc.getLongitude();
+		
+		str = "(Latitude: " + lat + ", longitude : "+lng +")";
+		
+		Geocoder gCoder = new Geocoder(this, Locale.getDefault());
+		
+		try {
+			List<Address> addrList = gCoder.getFromLocation(lat, lng, 1);
+			StringBuilder strBuilder = new StringBuilder();
+			if(addrList.size()>0){
+				Address addr = addrList.get(0);
+				for(int i=0;i<addr.getMaxAddressLineIndex();i++){
+					strBuilder.append(addr.getAddressLine(i)).append("\n");
+					strBuilder.append(addr.getLocality()).append("\n");
+					strBuilder.append(addr.getCountryName());
+					addrStr = strBuilder.toString();
+				}
+			}else{
+				addrStr = "Can't find Address"; 
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
+		LocTV.setText("location : "+ str + "\n\n Address : "+addrStr);
 	}
-
 }
